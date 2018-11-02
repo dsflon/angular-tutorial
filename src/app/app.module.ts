f2d43390eefb2afbbe506ec27953ea30b9b291ae
Daiki Saito <Daiki.Saito@isobar.com>
2018-11-02 17:32:42 +0900

    router設定

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,43 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
-import { MemberDetailComponent }  from './member-detail.component'
+import { DashboardComponent }  from './dashboard.component';
+import { MemberDetailComponent }  from './member-detail.component';
+import { MembersComponent }  from './members.component';
+import { MemberService } from './member.service';
+
+////
 
 @NgModule({
   imports:      [
       BrowserModule,
-      FormsModule
+      FormsModule,
+      RouterModule.forRoot([// routerの情報を設定
+          {
+              path: '', // リダイレクト設定
+              redirectTo: '/dashboard',
+              pathMatch: 'full' // full = 同じパスだった場合のみに有効
+          },
+          {
+              path: 'members', // /members にアクセスしたときに
+              component: MembersComponent // MembersComponentを表示する設定（さらにRouteroutletが必要）
+          },
+          {
+              path: 'dashboard',
+              component: DashboardComponent
+          }
+      ])
   ],
   declarations: [
       AppComponent,
-      MemberDetailComponent
+      DashboardComponent,
+      MemberDetailComponent,
+      MembersComponent
   ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [ AppComponent ],
+  providers: [ MemberService ]
 })
 export class AppModule { }
